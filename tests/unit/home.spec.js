a7e99dce832ff61d2f3783ec6a89657b031d91f3
Yannick <yannick.buettler@gmx.de>
2022-01-10 17:35:20 +0100

    some changes

--- tests/unit/home.spec.js
+++ tests/unit/home.spec.js
@@ -1,15 +1,15 @@
-// import { mount } from '@vue/test-utils'
-// import Home from '@/views/Home.vue'
-//
-// describe('Testing Home.vue', () => {
+import { mount } from '@vue/test-utils'
+import Home from '@/views/Home.vue'
+
+describe('Testing Home.vue', () => {
 //   it('renders props.msg when passed', () => {
 //     const msg = 'Welcome to the Budget-App!'
 //     const wrapper = mount(Home)
 //     expect(wrapper.text().substring(0, 26)).toMatch(msg)
 //   })
-// })
+})
 //
-// describe('Testing CreateAccount function', () => {
+describe('Testing CreateAccount function', () => {
 //   it('should not show form by default', () => {
 //     // when
 //     const wrapper = mount(Home)
@@ -17,4 +17,4 @@
 //     // then
 //     expect(wrapper.find('#account-create-offcanvas').classes()).not.toContain('show')
 //   })
-// })
+})
